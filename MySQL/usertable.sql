4c3def4c8b6fcba1435fe9eabfc80c246f3fd95f
ThijmHacks <thijmen.theloosen@outlook.com>
2024-08-23 23:09:45 +0200

    Created the first ever files for the sql connection.

---
+++ MySQL/usertable.sql
@@ -0,0 +1,10 @@
+USE chatod;
+
+CREATE TABLE users (
+    id INT(11) AUTO_INCREMENT PRIMARY KEY,
+    username VARCHAR(50) NOT NULL UNIQUE,
+    full_name VARCHAR(100) NOT NULL,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    phone VARCHAR(15),
+    password VARCHAR(255) NOT NULL
+);
